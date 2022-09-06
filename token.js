f2e4d48b9b453772046fe796a3759ed7a7257e32
БЕГИМ <zarlykovabegim02@gmail.com>
2022-09-06 22:05:46 +0600

    Added documentation to the bot

--- token.js
+++ token.js
@@ -1,5 +1,5 @@
 const { Telegraf } = require('telegraf')
-const bot = new Telegraf('5792085499:AAHEpBHfNuuUk8MnRCuhPs6F0MmPKhJ04K0') //
+const bot = new Telegraf('') // 
 console.log("In this bot you can find out more information about your sign and what we are waiting for you today")
 bot.launch()
 module.exports = bot
